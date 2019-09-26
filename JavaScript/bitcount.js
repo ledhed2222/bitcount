14e4a648f9df1ec84cfd39018279d7523173b8d5
Greg Weisbrod <greg.weisbrod@gmail.com>
2019-09-26 05:08:26 -0400

    make js example a node tool so it's better

--- JavaScript/bitcount.js
+++ JavaScript/bitcount.js
@@ -1,6 +1,23 @@
-var bitcount = function(n) {
-	var count = 0;
-	for (var i = 0; i < 32; i++) {
+#! /usr/bin/env node
+const main = () => {
+  const raw = process.argv[2]
+  if (!raw) {
+    console.log('You must enter a number!');
+    process.exit(1);
+  }
+
+  const arg = parseInt(raw);
+  if (isNaN(arg)) {
+    console.log(`${raw} is not a number!`);
+    process.exit(1);
+  }
+
+  console.log(bitcount(arg));
+}
+
+const bitcount = (n) => {
+	let count = 0;
+	for (let i = 0; i < 32; i++) {
 		if (n & 1 === 1) {
 			count++;
 		}
@@ -8,3 +25,6 @@ var bitcount = function(n) {
 	}
 	return count;
 };
+
+main();
+
