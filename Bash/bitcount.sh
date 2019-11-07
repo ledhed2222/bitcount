79578a0b590b031a8c6aea1619cc334d74f4e479
Greg Weisbrod <greg.weisbrod@gmail.com>
2019-11-07 06:39:22 -0500

    add case of non-number to bash

--- Bash/bitcount.sh
+++ Bash/bitcount.sh
@@ -2,16 +2,24 @@
 input=$1
 if [[ -z "$input" ]]
 then
-	echo "You must enter a number!"
-	exit
+  echo "You must enter a number!"
+  exit 1
 fi
+
+if ! [[ $input =~ ^-?[0-9]+$ ]]
+then
+  echo "${input} is not a number!"
+  exit 1
+fi
+
 count=0
 for i in {0..31}
 do
-	if (( $input & 1 ))  
-	then
-		let "count += 1"
-	fi
-	let "input = input >> 1"
+  if (( $input & 1 ))  
+  then
+    let "count += 1"
+  fi
+  let "input = input >> 1"
 done
 echo $count
+
